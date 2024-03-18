e0813d14ae4152090f12d983e0f922a7fa7997bc
Pruthvi <pruthvichippa@gmail.com>
2024-03-18 19:21:32 +0530

    testing

--- Base.java
+++ Base.java
@@ -3,4 +3,5 @@ public class Base {
     //this comment is added in the main branch
     private final String hello="helloe";
     private static final String variable="jack";
+    private static final String conflict="create";
 }
